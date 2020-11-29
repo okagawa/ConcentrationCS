c5d85efe1c82eb57a0db0aaf2a94f8357329dc16
Hiroshi Okagawa <109455+okagawa@users.noreply.github.com>
2020-11-29 16:45:17 +0900

    initial commit

---
+++ ConcentrationCS/ViewController.cs
@@ -0,0 +1,70 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace ConcentrationCS
+{
+    public partial class ViewController : UIViewController
+    {
+        private static int flipCount = 0;
+        public int FlipCount
+        {
+            set
+            {
+                flipCount = value;
+                flipCountLabel.Text = String.Format("Flips: {0}", flipCount);
+            }
+            get
+            {
+                return flipCount;
+            }
+        }
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void touchCard(UIButton sender)
+        {
+            FlipCount = FlipCount+1;
+
+            flipCard("👻", sender);
+            //throw new NotImplementedException();
+        }
+
+        partial void touchCard2(UIButton sender)
+        {
+            FlipCount = FlipCount + 1;
+
+            flipCard("🎃", sender);
+            // throw new NotImplementedException();
+        }
+
+        public void flipCard(String emoji, UIButton button)
+        {
+            if( button.CurrentTitle == emoji)
+            {
+                button.SetTitle("", UIControlState.Normal);
+                button.BackgroundColor = UIColor.Orange;
+            } else
+            {
+                button.SetTitle(emoji, UIControlState.Normal);
+                button.BackgroundColor = UIColor.White;
+            }
+        }
+
+    }
+}
---
+++ ConcentrationCS/ViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ConcentrationCS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel flipCountLabel { get; set; }
+
+        [Action ("touchCard:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void touchCard (UIKit.UIButton sender);
+
+        [Action ("touchCard2:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void touchCard2 (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Button != null) {
+                Button.Dispose ();
+                Button = null;
+            }
+
+            if (flipCountLabel != null) {
+                flipCountLabel.Dispose ();
+                flipCountLabel = null;
+            }
+        }
+    }
+}
